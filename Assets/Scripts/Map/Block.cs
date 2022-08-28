955d3eebd5efe67e3507fa045c8f5367b39ce428
WenkSo <wenso.new@gmail.com>
2022-08-28 15:34:22 +0800

    8.28

---
+++ Assets/Scripts/Map/Block.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Block : MonoBehaviour
+{
+    public Block Up;
+    public Block Down;
+    public Block Left;
+    public Block Right;
+
+    public BlockType type;
+}
---
+++ Assets/Scripts/Map/BlockType.cs
@@ -0,0 +1,6 @@
+public enum BlockType
+{
+    Plain,
+    Chance,
+    Luck
+}
