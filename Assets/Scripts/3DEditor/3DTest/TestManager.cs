7485dd5a1c7ae32209d16a32dafa2733cf62fcd0
Wenkang Su <34702081+WenKSo@users.noreply.github.com>
2022-12-29 18:02:03 +0800

    12/29

--- Assets/Scripts/3DEditor/3DTest/TestManager.cs
+++ Assets/Scripts/3DEditor/3DTest/TestManager.cs
@@ -1,9 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
+using System;
 
 public class TestManager : MonoBehaviour
 {
+    public GameObject plain;
     private void Start()
     {
 
@@ -11,6 +14,35 @@ public class TestManager : MonoBehaviour
 
     private void Load()
     {
+        //Load
+        if (File.Exists(Application.dataPath + "/save.txt"))
+        {
+            string saveString = File.ReadAllText(Application.dataPath + "/save.txt");
+            Block3D_Data[] blockData = JsonHelper.FromJson<Block3D_Data>(saveString);
+            for(int i=0;i<blockData.Length;i++)
+            {
+                GameObject createdObject = Instantiate(plain, blockData[i].position, Quaternion.identity);
+                createdObject.GetComponent<Block3D>().id = blockData[i].id;
+            }
 
+            GameObject[] blocks = GameObject.FindGameObjectsWithTag("Block");  
+            for(int i=0;i<blocks.Length;i++)
+            {
+                for(int j=0;j<blocks.Length;j++)
+                {
+                    if(blocks[j].GetComponent<Block3D>().id == blockData[i].nextId)
+                    {
+                        blocks[i].GetComponent<Block3D>().next = blocks[j].GetComponent<Block3D>();
+                    }
+                }
+            }
+        }
+    }
+
+    [Serializable]
+    private class Block3D_Data {
+        public Vector3 position;
+        public int id;
+        public int nextId;
     }
 }
