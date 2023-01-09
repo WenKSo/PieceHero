e4d336122eaff9a9d34cb3e654b821c0da176d93
Wenkang Su <wenso.new@gmail.com>
2023-01-09 10:40:56 +0800

    1/7

--- Assets/Scripts/3DEditor/EditorCanvas.cs
+++ Assets/Scripts/3DEditor/EditorCanvas.cs
@@ -3,11 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 using Fusion;
 using FusionUtilsEvents;
+using TMPro;
 using UnityEngine.SceneManagement;
 
 
 public class EditorCanvas : MonoBehaviour
 {
+    private GameMode _gameMode;
+
+    public string Nickname = "Player";
+    public GameLauncher Launcher;
+
     public FusionEvent OnPlayerJoinedEvent;
     public FusionEvent OnPlayerLeftEvent;
     public FusionEvent OnShutdownEvent;
@@ -20,6 +26,9 @@ public class EditorCanvas : MonoBehaviour
     [Space]
     [SerializeField] private GameObject _infoEnter;
     [SerializeField] private GameObject _chooseMode;
+    [Space]
+    [SerializeField] private TMP_InputField _nickname;
+    [SerializeField] private TMP_InputField _room;
 
     public void RunButton()
     {
@@ -42,5 +51,21 @@ public class EditorCanvas : MonoBehaviour
     public void StartButton()
     {
         SceneManager.LoadScene (sceneName:"3DTest");
+    }
+
+    //Called from button
+    public void StartLauncher()
+    {
+        Launcher = FindObjectOfType<GameLauncher>();
+        Nickname = _nickname.text;
+        PlayerPrefs.SetString("Nick", Nickname);
+        Launcher.Launch(_gameMode, _room.text);
+        _nickname.transform.parent.gameObject.SetActive(false);
+    }
+
+    //Called from button
+    public void ExitGame()
+    {
+        GameManager.Instance.ExitGame();
     }
 }
